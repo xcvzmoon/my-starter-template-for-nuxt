de89b8c0106320d429a02cb2ff4931740bd8addd
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2025-08-08 15:38:01 +0800

    chore: add vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2025-08-08',
 
   css: ['~/assets/css/main.css'],
 
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@vueuse/nuxt'],
 
   colorMode: {
     preference: 'dark',
